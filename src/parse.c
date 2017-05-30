b454e51b7d685fdeef87c434dc841dbbf0cc023c
Simon Petit <simon@MacBook-Pro-de-Simon.local>
2017-05-30 21:50:34 +0200

    testing

---
+++ src/parse.c
@@ -0,0 +1,7 @@
+#include <limits.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void readline(char* cmdLine) {
+
+}
---
+++ src/psh.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+
+int main(void) {
+	char* cwd=NULL;
+	char buff[PATH_MAX+1];
+	cwd = getcwd(buff, PATH_MAX+1);
+	if (cwd != NULL) {
+		printf("%s",cwd);
+		printf("\n");
+	}
+	
+    char cmdLine[PATH_MAX];
+    fgets(cmdLine, sizeof(cmdLine), stdin);
+    printf("%s", cmdLine);
+    
+    char *arguments[] = { "ls", NULL };
+    
+    if (execv("/bin/ls",arguments)==-1){
+        return EXIT_FAILURE;
+    };
+	return EXIT_SUCCESS;
+}
