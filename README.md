26d4b4828d4639c0a95d0f742d56974d69a8eb67
sopiU6 15 <sopiu615@MacBook-Air-de-sopiU6.local>
2017-06-01 12:53:44 +0200

    completing readme + forking in command.c

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 PSH is for PetitSHell, my own hand made shell in C.
 Nothing fancy for now, but it may evolve at a relative speed.
 This self project is first of all my own challenge. Let's see how it goes.
+To use the psh right now:
+    Clone the repo,
+    make build to compile with gcc
+    ./psh to run
--- src/command.c
+++ src/command.c
@@ -1,7 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "command.h"
 
 int command(char **args) {
-    return execvp(args[0], args);
+    pid_t pid;
+    pid_t wait;
+    int status;
+
+    pid = fork();
+    if ( pid == 0 ) {
+        execvp(args[0], args);
+    } else if ( pid > 0 ) {
+        do {
+            wait = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    } else {
+        return 0;
+    }
+
+    return 1;
 }
